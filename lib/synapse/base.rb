93c8a3a368a659c80f1bed3d22ed2595955cef66
Igor Serebryany <igor.serebryany@airbedandbreakfast.com>
2012-11-28 18:22:48 -0800

    add utility safe_run method

--- lib/synapse/base.rb
+++ lib/synapse/base.rb
@@ -1,14 +1,17 @@
 module Synapse
-
-
   def log(message)
     @@log = Logger.new STDOUT unless defined?(@@log)
     @@log.debug message
   end
 
-  class Base
+  def safe_run(command)
+    res = `command`.chomp
+    raise "command '#{command}' failed to run:\n#{res}" unless $?.success?
+  end
 
+  class Base
     def initialize
+      super
     end
   end
 end
