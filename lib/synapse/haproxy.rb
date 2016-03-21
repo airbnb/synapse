42c4d488b692f46697bd46abd4d184c3a76b25ec
Tiffany Low <tiffany.low@airbnb.com>
2016-03-21 11:43:56 -0700

    Restart haproxy if backend 'weights' setting is changed

--- lib/synapse/haproxy.rb
+++ lib/synapse/haproxy.rb
@@ -714,6 +714,11 @@ module Synapse
             log.info "synapse: restart required because haproxy_server_options changed for #{backend_name}"
             @restart_required = true
           end
+          if (old_backend.fetch('weight', "") !=
+              backend.fetch('weight', ""))
+            log.info "synapse: restart required because weight changed for #{backend_name}"
+            @restart_required = true
+          end
         end
         backends[backend_name] = backend.merge('enabled' => true)
       end
@@ -749,9 +754,12 @@ module Synapse
           end
           b = "#{b} #{watcher.haproxy['server_options']}" if watcher.haproxy['server_options']
           if backend.has_key?('weight')
-            # Check if server_options already contains weight, is so log a warning
-            if watcher.haproxy['server_options'].include? 'weight'
-              log.warn "synapse: weight is defined by server_options and by nerve"
+            # Check if server_options/haproxy_server_options already contains weight, if so log a warning
+            if watcher.haproxy.fetch('server_options', '').include? 'weight'
+              log.warn "synapse: weight is defined in both server_options and nerve. nerve weight takes precedence"
+            end
+            if backend['haproxy_server_options'] and backend['haproxy_server_options'].include? 'weight'
+              log.warn "synapse: weight is defined in both haproxy_server_options and nerve. nerve weight takes precedence"
             end
             weight = backend['weight'].to_i
             b = "#{b} weight #{weight}"
