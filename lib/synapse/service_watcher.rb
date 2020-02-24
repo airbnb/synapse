01fe68f8bb1dea64ad49fd4946bec68cfa76fcfd
Rushy Panchal <rushy.panchal@airbnb.com>
2020-02-24 12:55:42 -0800

    Factor out watcher loader from service_watcher

--- lib/synapse/service_watcher.rb
+++ lib/synapse/service_watcher.rb
@@ -11,15 +11,20 @@ module Synapse
         unless opts.has_key?('discovery') && opts['discovery'].has_key?('method')
 
       discovery_method = opts['discovery']['method']
+      return self.load_watcher(discovery_method, opts, synapse)
+    end
+
+    def self.load_watcher(discovery_method, opts, synapse)
       watcher = begin
-        method = discovery_method.downcase
-        require "synapse/service_watcher/#{method}"
-        # zookeeper_dns => ZookeeperDnsWatcher, ec2tag => Ec2tagWatcher, etc ...
-        method_class  = method.split('_').map{|x| x.capitalize}.join.concat('Watcher')
-        self.const_get("#{method_class}")
-      rescue Exception => e
-        raise ArgumentError, "Specified a discovery method of #{discovery_method}, which could not be found: #{e}"
-      end
+                  method = discovery_method.downcase
+                  require "synapse/service_watcher/#{method}"
+                  # zookeeper_dns => ZookeeperDnsWatcher, ec2tag => Ec2tagWatcher, etc ...
+                  method_class  = method.split('_').map{|x| x.capitalize}.join.concat('Watcher')
+                  self.const_get("#{method_class}")
+                rescue Exception => e
+                  raise ArgumentError, "Specified a discovery method of #{discovery_method}, which could not be found: #{e}"
+                end
+
       return watcher.new(opts, synapse)
     end
   end
