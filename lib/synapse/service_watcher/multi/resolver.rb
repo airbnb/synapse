ff9d181a89d247cbd29972662392322de9d02cf8
Rushy Panchal <rushy.panchal@airbnb.com>
2020-03-02 15:25:14 -0800

    Add loader for resolvers

---
+++ lib/synapse/service_watcher/multi/resolver.rb
@@ -0,0 +1,18 @@
+class Synapse::ServiceWatcher::MultiWatcher
+  class Resolver
+    def self.load_resolver(opts, watchers)
+      raise ArgumentError, "resolver method not provided" unless opts.has_key?('method')
+      method = opts['method'].downcase
+
+      resolver = begin
+                   require "synapse/service_watcher/multi/resolver/#{method}"
+                   method_class  = method.split('_').map{|x| x.capitalize}.join.concat('Resolver')
+                   self.const_get("#{method_class}")
+                 rescue Exception => e
+                   raise ArgumentError, "Specified a resolver method of #{method}, which could not be found: #{e}"
+                 end
+
+      return resolver.new(opts, watchers)
+    end
+  end
+end
---
+++ spec/lib/synapse/multi_resolver/loader_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+require 'synapse/service_watcher/base/base'
+require 'synapse/service_watcher/multi/resolver'
+require 'synapse/service_watcher/multi/resolver/base'
+
+describe Synapse::ServiceWatcher::MultiWatcher::Resolver do
+  subject { Synapse::ServiceWatcher::MultiWatcher::Resolver }
+
+  let(:watchers) {
+    [
+      instance_double(Synapse::ServiceWatcher::BaseWatcher)
+    ]
+  }
+
+  describe ".load_resolver" do
+    let(:config) { {'method' => 'base'} }
+    subject {
+      Synapse::ServiceWatcher::MultiWatcher::Resolver
+    }
+
+    context 'with method => base' do
+      it 'creates the base resolver' do
+        expect(subject::BaseResolver).to receive(:new).exactly(:once).with(config, watchers)
+        expect { subject.load_resolver(config, watchers) }.not_to raise_error
+      end
+    end
+
+    context 'with bogus method' do
+      let(:config) { {'method' => 'bogus'} }
+
+      it 'raises an error' do
+        expect(subject::BaseResolver).not_to receive(:new)
+        expect { subject.load_resolver(config, watchers) }.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
